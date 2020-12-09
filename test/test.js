250aacf713fdb34ccbf4542548841ed32517e54f
yama-dev <tatsuya.yamamoto69@gmail.com>
2020-12-09 14:18:20 +0900

    adjust test.

--- test/test.js
+++ test/test.js
@@ -5,8 +5,8 @@ const dataCollector = require('../dataCollector.js');
 
 let stdout_data = null;
 
-describe('Run function', function() {
-  it('Run.', function() {
+describe('Run "order:DESC"', function() {
+  it('set data.', function() {
     try {
       stdout_data = dataCollector({
         data: 'data',
@@ -20,11 +20,11 @@ describe('Run function', function() {
 
   describe('check `all` data', function() {
     it('length', function() {
-      assert.strictEqual(2, stdout_data.all.length);
+      assert.strictEqual(3, stdout_data.all.length);
     });
     it('title', function() {
       assert.strictEqual('ウェブサイトを開設しました', stdout_data.all[0].title);
-      assert.strictEqual('json sample', stdout_data.all[1].title);
+      assert.strictEqual('sample json', stdout_data.all[1].title);
     });
   });
 
@@ -34,7 +34,47 @@ describe('Run function', function() {
     });
     it('title', function() {
       assert.strictEqual('ウェブサイトを開設しました', stdout_data.news[0].title);
-      assert.strictEqual('json sample', stdout_data.news[1].title);
+      assert.strictEqual('sample json', stdout_data.news[1].title);
+    });
+  });
+
+  after(function(done) {
+    // console.log('data \n');
+    // console.log(stdout_data);
+    done();
+  });
+});
+
+describe('Run "order:ASC"', function() {
+  it('set data.', function() {
+    try {
+      stdout_data = dataCollector({
+        data: 'data',
+        order: 'ASC',
+        orderby: 'date',
+      });
+    } catch (e) {
+      throw e;
+    }
+  });
+
+  describe('check `all` data', function() {
+    it('length', function() {
+      assert.strictEqual(3, stdout_data.all.length);
+    });
+    it('title', function() {
+      assert.strictEqual('sample xml', stdout_data.all[0].title);
+      assert.strictEqual('sample json', stdout_data.all[1].title);
+    });
+  });
+
+  describe('check `news` data', function() {
+    it('length', function() {
+      assert.strictEqual(2, stdout_data.news.length);
+    });
+    it('title', function() {
+      assert.strictEqual('sample json', stdout_data.news[0].title);
+      assert.strictEqual('ウェブサイトを開設しました', stdout_data.news[1].title);
     });
   });
 
