7a826c28b00668c68e507cc47971bce5e57acf02
yama-dev <tatsuya.yamamoto69@gmail.com>
2020-12-11 18:40:08 +0900

    adjust test

--- test/test.js
+++ test/test.js
@@ -11,7 +11,7 @@ describe('Run "order:DESC"', function() {
       stdout_data = dataCollector({
         data: 'data',
         order: 'DESC',
-        orderby: 'date',
+        orderby: 'order',
       });
     } catch (e) {
       throw e;
@@ -20,11 +20,11 @@ describe('Run "order:DESC"', function() {
 
   describe('check `all` data', function() {
     it('length', function() {
-      assert.strictEqual(3, stdout_data.all.length);
+      assert.strictEqual(4, stdout_data.all.length);
     });
     it('title', function() {
-      assert.strictEqual('ウェブサイトを開設しました', stdout_data.all[0].title);
-      assert.strictEqual('sample json', stdout_data.all[1].title);
+      assert.strictEqual('最新情報をチェック！', stdout_data.all[0].title);
+      assert.strictEqual('ウェブサイトを開設しました', stdout_data.all[1].title);
     });
   });
 
@@ -35,6 +35,15 @@ describe('Run "order:DESC"', function() {
     it('title', function() {
       assert.strictEqual('ウェブサイトを開設しました', stdout_data.news[0].title);
       assert.strictEqual('sample json', stdout_data.news[1].title);
+    });
+  });
+
+  describe('check `blog` data', function() {
+    it('length', function() {
+      assert.strictEqual(1, stdout_data.blog.length);
+    });
+    it('title', function() {
+      assert.strictEqual('最新情報をチェック！', stdout_data.blog[0].title);
     });
   });
 
@@ -51,7 +60,7 @@ describe('Run "order:ASC"', function() {
       stdout_data = dataCollector({
         data: 'data',
         order: 'ASC',
-        orderby: 'date',
+        orderby: 'order',
       });
     } catch (e) {
       throw e;
@@ -60,7 +69,7 @@ describe('Run "order:ASC"', function() {
 
   describe('check `all` data', function() {
     it('length', function() {
-      assert.strictEqual(3, stdout_data.all.length);
+      assert.strictEqual(4, stdout_data.all.length);
     });
     it('title', function() {
       assert.strictEqual('sample xml', stdout_data.all[0].title);
@@ -75,6 +84,15 @@ describe('Run "order:ASC"', function() {
     it('title', function() {
       assert.strictEqual('sample json', stdout_data.news[0].title);
       assert.strictEqual('ウェブサイトを開設しました', stdout_data.news[1].title);
+    });
+  });
+
+  describe('check `blog` data', function() {
+    it('length', function() {
+      assert.strictEqual(1, stdout_data.blog.length);
+    });
+    it('title', function() {
+      assert.strictEqual('最新情報をチェック！', stdout_data.blog[0].title);
     });
   });
 
